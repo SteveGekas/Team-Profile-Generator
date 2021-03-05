26f985b6b1415197380f571583974d96629a298e
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-03-04 21:19:24 -0500

    added JS code

--- Engineer.js
+++ Engineer.js
@@ -1,3 +1,20 @@
-class Engineer extends Employee {
-    
+const employee = require('Employee');
+
+class engineer extends employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+
+
+    }
+
+    getGithub() {
+        return this.github;
+    }
+
+    getRole() {
+        return 'Engineer';
+    }
 }
+
+module.exports = engineer;
