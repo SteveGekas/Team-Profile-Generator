c4475042e2c2c944f2be2dc38251a9ebef2334e2
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-03-04 21:25:04 -0500

    added JS code

--- Intern.js
+++ Intern.js
@@ -1,3 +1,20 @@
-class Intern extends Employee {
-    
+const employee = require('Employee');
+
+class intern extends employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+
+
+    }
+
+    getSchool() {
+        return this.school;
+    }
+
+    getRole() {
+        return 'Intern';
+    }
 }
+
+module.exports = intern;
