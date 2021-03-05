83db50ac33057f3c6033fcc557fb35e64a1f7172
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-03-04 21:15:52 -0500

    added JS code

--- Manager.js
+++ Manager.js
@@ -1,3 +1,20 @@
-class Manager extends Employee {
-    
- }
+const employee = require('Employee');
+
+class manager extends employee {
+    constructor(name, id, email, officeNum) {
+        super(name, id, email);
+        this.officeNum = officeNum;
+
+
+    }
+
+    getOfficeNum() {
+        return this.officeNum;
+    }
+
+    getRole() {
+        return 'Manager';
+    }
+}
+
+module.exports = manager;
